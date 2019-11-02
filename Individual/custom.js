261826897015a430e86df4eda796045aa54661f9
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2019-11-02 19:25:40 +0100

    javascript validation fix

--- Individual/custom.js
+++ Individual/custom.js
@@ -1,14 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    if(window.innerWidth > 1280){
-        console.log('XL Query');
-    } else if(window.innerWidth <= 1280 && window.innerWidth > 1024){
-        console.log('LG Query');
-    } else if(window.innerWidth <= 1024 && window.innerWidth > 768){
-        console.log('MD Query');
-    } else if(window.innerWidth <= 768 && window.innerWidth > 640){
-        console.log('SM Query');
-    }else{
-        console.log('XS Query')
-    }
-    console.log(window.innerWidth, 'px');
-});
+alert('Hello')
+function validate ()
+{
+    var name = document.getElementById("uname")
+    var email = document.getElementById("email")
+    var phone = document.getElementById("phone")
+    var subject = document.getElementById("subject")
+  
+  if(name.value =="" ||password.value=="")
+{
+    alert("No blank values allowed");
+    return false; 
+}
+else
+{
+    true;   
+}
+}
--- Individual/portal.js
+++ Individual/portal.js
@@ -1,1 +1,18 @@
-alert('Hello')
+function validate()
+{
+    var name = document.getElementById("uname")
+    var email = document.getElementById("email");
+    var phone = document.getElementById("phone");
+    var subject = document.getElementById("subject");
+    var subject = document.getElementById("message");
+
+    if(name.value =="" && email.value =="" && phone.value =="" && subject.value =="" ||message.value =="")
+    {
+    alert("No blank values allowed");
+        return false;
+    }
+    else
+    {
+        true;
+    }
+}
